b12c2c59a09f680c0530a487f6bc4d3cda5fcd60
Javier Velasquez <javelasquezb.d.17@gmail.com>
2022-06-03 06:39:43 -0400

    Add project files.

---
+++ ConnectedCarPrismExample/ConnectedCarPrismExample/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Prism.Commands;
+using Prism.Navigation;
+using System;
+
+namespace ConnectedCarPrismExample.ViewModels
+{
+    public class LoginPageViewModel : ViewModelBase
+    {
+        private readonly INavigationService _navigationService;
+        private string _userName;
+        private string _password;
+
+        private DelegateCommand _loginCommand;
+
+        public string UserName
+        {
+            get => _userName;
+            set => SetProperty(ref _userName, value);
+        }
+
+        public string Password
+        {
+            get => _password;
+            set => SetProperty(ref _password, value);
+        }
+
+        public DelegateCommand LoginCommand => _loginCommand ?? (_loginCommand = new DelegateCommand(Login));
+
+
+        private async void Login()
+        {
+            await App.Current.MainPage.DisplayAlert("Hola", "Te saludo desde el comando", "ok");
+        }
+
+        public LoginPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            _navigationService = navigationService;
+        }
+    }
+}
